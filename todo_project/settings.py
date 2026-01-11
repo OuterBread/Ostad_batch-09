7ab15ee7aeba0328c7d679ca480d8c02a4a1b4e2
Md.Latifur Rahman Rafi <133046039+latifurrafi@users.noreply.github.com>
2026-01-11 22:59:35 +0600

    Fix formatting of allowed host in settings

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
     'subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     '.ngrok-free.dev',  # Allow all ngrok-free.dev subdomains
     '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
-    'testtttttt-kf9v.onrender.com'
+    'testtttttt-kf9v.onrender.com',
     'ostad-batch-09-1-cwz9.onrender.com',
 ]
 
